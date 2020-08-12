45ebea32b074a93db5317897256f697fffaa7dc2
takumidev86 <takumidevelopment86@gmail.com>
2020-08-12 15:04:12 +0900

    \

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'books#top'
   get 'top' => 'books#top'
   get 'show2' => 'users#show2'
-  get 'about' => 'books#about', as: 'home_about'
+#   get 'about' => 'books#about', as: 'home_about'
+  get 'home/about' => 'books#about', as: 'home_about'
+
   resources :books, expect: [:new]
   patch 'books' => 'books#create'
   resources :users, only: [:show, :edit, :update, :index]
