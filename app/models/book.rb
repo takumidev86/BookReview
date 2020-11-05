0485a7f467aface0ac71011ebd725cd2bbdad630
takumidev86 <takumidevelopment86@gmail.com>
2020-11-05 06:24:55 +0000

    [Finish] favorite

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ApplicationRecord
-	belongs_to :user
+    belongs_to :user
+    
 	validates :title, presence: true
-	validates :body, presence: true, length: { maximum: 200 }
+    validates :body, presence: true, length: { maximum: 200 }
+
+    has_many :favorites, dependent: :destroy
+    
+    def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
 end
