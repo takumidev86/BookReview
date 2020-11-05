5d65c21e3cb2a4f8fdf516a0495c8a3ceafadcfa
takumidev86 <takumidevelopment86@gmail.com>
2020-11-05 06:27:47 +0000

    [Finish] favorites #1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-
+# いいね機能
     def create
         @book = Book.find(params[:book_id])
         favorite = current_user.favorites.new(book_id: @book.id)
