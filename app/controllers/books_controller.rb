591cc47ea52783870793b756a0e2014aa0b98495
takumidev86 <takumidevelopment86@gmail.com>
2020-11-05 08:46:10 +0000

    [fix] books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:top, :about]
     before_action :ensure_correct_user, only: [:edit, :update, :destroy]
     
 	def top
